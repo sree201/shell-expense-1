a95bc79ada2dd05f5666c3d7e7d83b6fd4c67e43
Sree201 <sreesirinath4@gmail.com>
2024-06-10 23:16:24 -0700

    backend

--- mysql.sh
+++ mysql.sh
@@ -1,3 +1,31 @@
+# #!/bin/bash
+
+# source ./common.sh
+
+# check_root
+
+# echo "Please enter DB password:"
+# read -s mysql_root_password
+
+# dnf install mysql-server -y &>>$LOGFILE
+
+# systemctl enable mysqld &>>$LOGFILE
+
+# systemctl start mysqld &>>$LOGFILE
+
+# #mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+# #VALIDATE $? "Setting up root password"
+
+# #Below code will be useful for idempotent nature
+# mysql -h 172.31.22.201 -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+# if [ $? -ne 0 ]
+# then
+#     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+    
+# else
+#     echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
+# fi
+
 #!/bin/bash
 
 source ./common.sh
@@ -13,15 +41,11 @@ systemctl enable mysqld &>>$LOGFILE
 
 systemctl start mysqld &>>$LOGFILE
 
-#mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-#VALIDATE $? "Setting up root password"
-
 #Below code will be useful for idempotent nature
 mysql -h 172.31.22.201 -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
-    
 else
     echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
 fi
